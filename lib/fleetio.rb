a38cdccf5f4c8bf2d0c1d94ead2f60f8532d10e9
Air Fleetio <55637358+airfleetio@users.noreply.github.com>
2019-09-19 20:22:09 -0700

    Add basic HTTP client for Fleet.io API
    In a full-fledged application, the API client would be separated
    into multiple files with different methods. For now, one module
    will do. We can split into successive commits.
    
    The credentials to access the API are stored using Rails credentials.
    In order to access them you need a copy of the master key.
    
    Note that this version of the API client is not concerned with
    pagination and is only fetching the first page of vehicles.
    We will address this improvement in a separate commit.

---
+++ lib/fleetio.rb
@@ -0,0 +1,38 @@
+require 'rest-client'
+
+# A basic client for the fleet.io API
+module Fleetio
+  class Vehicle
+    attr_reader :uuid, :vin, :make, :model, :year
+
+    def initialize(params = {})
+      @uuid = params[:id]
+      @vin = params[:vin]
+      @make = params[:make]
+      @model = params[:model]
+      @year = params[:year]
+    end
+
+    def self.all
+      Request.fetch(path: '/vehicles').map do |vehicle_attributes|
+        new vehicle_attributes
+      end
+    end
+  end
+
+  class Request
+    def self.fetch(path:)
+      url = "https://secure.fleetio.com/api/v1#{path}"
+      response = RestClient.get url, headers
+      JSON.parse response.body, symbolize_names: true
+    end
+
+  private
+
+    def self.headers
+      auth_token = Rails.application.credentials.dig :fleetio, :auth_token
+      account_token = Rails.application.credentials.dig :fleetio, :account_token
+      {'Authorization': %{Token token="#{auth_token}"}, 'Account-Token': account_token}
+    end
+  end
+end
